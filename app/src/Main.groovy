b132651bb167f3b147f432c01a74e971d4352ddd
Vinay Selvaraj <vinay@selvaraj.com>
2015-07-08 23:32:55 -0400

    Major re-work

--- app/src/Main.groovy
+++ app/src/Main.groovy
@@ -7,60 +7,93 @@ import com.amazonaws.services.ec2.model.*
 
 LOG = Logger.getInstance(getClass())
 
-def env = System.getenv()
-def cluster_name = env['CLUSTER_NAME']
+// Read cluster name from environment variable
+ENV          = System.getenv()
+CLUSTER_NAME = ENV['CLUSTER_NAME']
 
-if(cluster_name == null) {
+if(CLUSTER_NAME == null) {
   LOG.fatal("Environment variable CLUSTER_NAME cannot be null")
   System.exit(1)
 }
 
-def ecsClient = new AmazonECSClient()
+// Initialize AWS Clients
+ecsClient = new AmazonECSClient()
+ec2Client = new AmazonEC2Client()
 
-List taskArns = new ArrayList()
+// Get all tasks in the cluster
+def tasks = getTasksForCluster()
 
-// Get all task ARNs for the cluster
-def token = null
-def done = false
-while(!done) {
+// Get a map of container instance arns to container instances
+def containerInstanceMap = getContainerInstanceMap(tasks)
+
+// Get a map of ec2 instance ids to ec2 instances
+def ec2InstanceMap = getEc2InstanceMap(containerInstanceMap.values())
+
+// Get map of task definitions <TaskDefinitionArn, TaskDefinition>
+def taskDefintionMap = getTaskDefintionMap(tasks)
+
+
+def getTasksForCluster() {
   
-  ListTasksRequest listTasksRequest = new ListTasksRequest()
-  listTasksRequest.cluster = cluster_name
-  listTasksRequest.nextToken = token
+  // Get all task ARNs for the cluster
+  def taskArns = new ArrayList()
   
-  def tasks = ecsClient.listTasks(listTasksRequest)
-  taskArns.addAll(tasks.taskArns)
+  def token = null
+  def done = false
   
-  if(token == null) {
-    done = true
-  } else {
-    token = tasks.nextToken
+  while(!done) {  
+    ListTasksRequest listTasksRequest = new ListTasksRequest()
+    listTasksRequest.cluster = CLUSTER_NAME
+    listTasksRequest.nextToken = token
+  
+    def tasks = ecsClient.listTasks(listTasksRequest)
+    taskArns.addAll(tasks.taskArns)
+  
+    if(token == null) {
+      done = true
+    } else {
+      token = tasks.nextToken
+    }
   }
-}
-
-if(taskArns.size > 0) {
-
+  
   // Describe Tasks
   def descTasksRequest = new DescribeTasksRequest().withTasks(taskArns)
   def descTasksResult = ecsClient.describeTasks(descTasksRequest)
-  def tasks = descTasksResult.tasks
   
-  // Put task containers in a map
-  def taskContainerInstanceMap = new HashMap()
+  return descTasksResult.tasks
+}
+
+def getTaskDefintionMap(def tasks) {
+  def taskDefinitionArns = new ArrayList()
+  
   tasks.each { task->
-    def taskContainers = taskContainerInstanceMap.get(task.containerInstanceArn)
-    if(taskContainers == null) {
-      taskContainers = new ArrayList()
+    if(!taskDefinitionArns.contains(task.taskDefinitionArn)) {
+      taskDefinitionArns.add(task.taskDefinitionArn)
     }
-    taskContainers.addAll(task.containers)
-    
-    taskContainerInstanceMap.put(task.containerInstanceArn, taskContainers)
   }
   
+  // Put task definitions in a map <taskDefinitonArn, taskDefinition>
+  def taskDefinitionMap = new HashMap()
+  taskDefinitionArns.each { taskDefinitionArn->
+    def descTaskDefResult = ecsClient.describeTaskDefinition(
+      new DescribeTaskDefinitionRequest().withTaskDefinition(taskDefinitionArn))
+    taskDefinitionMap.put(taskDefinitionArn, descTaskDefResult.taskDefinition)
+  }
+  
+  return taskDefinitionMap
+}
+
+def getContainerInstanceMap(def tasks) {
+  
+  def containerInstanceArns = new ArrayList()
+  tasks.each { task->
+    containerInstanceArns.add(task.containerInstanceArn)
+  }
+
   // Get container instances
   def descContainerInstancesResult = ecsClient.describeContainerInstances(
     new DescribeContainerInstancesRequest()
-      .withContainerInstances(taskContainerInstanceMap.keySet()))
+      .withContainerInstances(containerInstanceArns))
   def containerInstances = descContainerInstancesResult.containerInstances
   
   // Put container instances in a HashMap to lookup by Arn
@@ -69,8 +102,12 @@ if(taskArns.size > 0) {
     containerInstanceMap.put(containerInstance.containerInstanceArn, containerInstance)
   }
   
+  return containerInstanceMap
+}
+
+def getEc2InstanceMap(def containerInstances) {
+  
   // Get EC2 instances for each container instance
-  def ec2Client = new AmazonEC2Client()
   def ec2InstanceIds = new ArrayList()
   containerInstances.each { containerInstance ->
     ec2InstanceIds.add(containerInstance.ec2InstanceId)
@@ -86,14 +123,6 @@ if(taskArns.size > 0) {
     }
   }
   
-  
-  taskContainerInstanceMap.keySet().each { taskContainerInstanceArn ->
-    def taskContainers = taskContainerInstanceMap.get(taskContainerInstanceArn)
-    def containerInstance = containerInstanceMap.get(taskContainerInstanceArn)
-    //println taskContainers
-    //println containerInstance
-    println ec2InstanceMap.get(containerInstance.ec2InstanceId).privateIpAddress
-  }
-  
+  return ec2InstanceMap
 }
 
